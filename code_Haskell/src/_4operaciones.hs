749b56bad1dd248bff05fca6209895a2d4688a67
jkenigma <wall.kennedy@hotmail.com>
2019-12-19 12:10:04 -0500

    agrega operaciones

---
+++ code_Haskell/src/_4operaciones.hs
@@ -0,0 +1,20 @@
+
+
+main = do
+ let numero1 = 223
+ let numero2 = 656
+ putStrLn  "la suma es "
+ print(numero1 + numero2)
+ 
+ putStrLn "la diferencia "
+ print(numero2 - numero1)
+ 
+ putStrLn "el producto "
+ print(numero2 * numero1)
+ 
+ let num1 = 1000
+ let num2 = 50
+ putStrLn "la division "
+ print(num1 / num2)
+
+
