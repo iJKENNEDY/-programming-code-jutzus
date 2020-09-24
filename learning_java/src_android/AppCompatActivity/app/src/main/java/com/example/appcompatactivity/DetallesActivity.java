e19fdae900f4a301897293ccd597cd1e6533bae7
enigma Godel <wall.kennedy@hotmail.com>
2020-09-23 23:36:37 -0500

    s3_android

---
+++ learning_java/src_android/AppCompatActivity/app/src/main/java/com/example/appcompatactivity/DetallesActivity.java
@@ -0,0 +1,21 @@
+package com.example.appcompatactivity;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetallesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+        getSupportActionBar().setTitle(getIntent().getExtras().get("Nombre").toString());
+
+
+    }
+
+
+}
